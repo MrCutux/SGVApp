a3a4f938a3e761578898bd4d732ebe47c80678f8
AlvaroMY <alv.madrid@duocuc.cl>
2021-12-06 05:02:01 -0300

    ultimos commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { IngresarGuard } from './ingresar.guard';
 import { NoIngresarGuard } from './noingresar.guard';
 
 const routes: Routes = [
@@ -17,28 +16,23 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./Pages/inicio/inicio.module').then( m => m.InicioPageModule),
-    //canActivate:[IngresarGuard]
   },
   
   {
     path: 'formularios',
     loadChildren: () => import('./Pages/formularios/formularios.module').then( m => m.FormulariosPageModule),
-    //canActivate:[IngresarGuard]
   },
   {
     path: 'ficha',
     loadChildren: () => import('./Pages/ficha/ficha.module').then( m => m.FichaPageModule),
-    //canActivate:[IngresarGuard]
   },
   {
     path: 'asistencia',
     loadChildren: () => import('./Pages/asistencia/asistencia.module').then( m => m.AsistenciaPageModule),
-    //canActivate:[IngresarGuard]
   },
   {
     path: '**',
     loadChildren: () => import('./Pages/not-found404/not-found404.module').then( m => m.NotFound404PageModule),
-    //canActivate:[IngresarGuard]
   },
 ];
 
